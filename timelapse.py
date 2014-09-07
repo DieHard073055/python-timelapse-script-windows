f2516329ea056b009d7f3de57fb94d7f3aed0aa2
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-09-07 22:07:01 +0800

    add(timelapse.py)

--- timelapse.py
+++ timelapse.py
@@ -16,3 +16,4 @@ for filename in os.listdir(pics_dir_proper):
 	imagenumber = imagenumber+1
 cmd = '..\\ffmpeg\\bin\\ffmpeg.exe -f image2 -i ' + new_dir + '\\%d.JPG -r 25 -s 960x640 -vcodec libx264 ' + outputname
 os.system(cmd)
+shutil.rmtree('..\\timelapsefootage')
